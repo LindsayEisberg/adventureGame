5cae74feeeeaf6e7f5e78b14118d1bea4ba073b6
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-15 13:17:03 -0500

    initial adventure game file setup

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Check if the user is ready to play!
+confirm ("Click ok if you are ready to play!");
+
+var age = prompt("What is your age?");
+if (age < 13) {
+  console.log("Ok, you can play, but I take no responsibility for any mischief that occurs.");
+}
+else {
+  console.log("Great! Let the adventure begin!");
+};
