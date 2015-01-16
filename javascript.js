ddb4f269bc987bb16a889dcc72e43de68c39955e
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-16 01:05:26 -0500

    js beginning for adventure draft

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
-// Check if the user is ready to play!
-confirm ("Click ok if you are ready to play!");
+var name = prompt("What is your name?");
+alert("Greetings "  + name + "!  Welcome to Adventures of Nash, The Dog");
 
-var age = prompt("What is your age?");
-if (age < 13) {
-  console.log("Ok, you can play, but I take no responsibility for any mischief that occurs.");
+
+function startGame() {
+var begin = prompt("Are you Ready to play (YES or NO)").toUpperCase();
+if (begin === "YES") {
+  alert("Great! Let's get started.");
 }
 else {
-  console.log("Great! Let the adventure begin!");
+  alert("That's too bad. This game is pretty fun. See you next time.");
+}
 };
+var nash = prompt("Nash is a dog. Dogs like squirrels. One day the front door was left open and Nash sees a squirrel! Being a dog she chases the squirrel. Choose to either CHASE Nash or STAY inside.");
+
+
+
+function startGame() {
+  var wannaPlay = confirm("do you want to play?");
+
+  if(wannaPlay) {
+    beginGame() // youll need to make this
+  } else {
+    return ();  // stops execution of program
+  }
+}
+Show less
+
+startGame()
